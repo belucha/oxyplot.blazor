e62431cd17b22b2b0b0ac8606d985b8bca3c94f9
Daniel Gross <dgross@intronik.de>
2020-11-04 07:24:56 +0100

    close #3: https://github.com/belucha/oxyplot.blazor/issues/3

--- OxyPlot.Blazor/wwwroot/OxyPlot.Blazor.js
+++ OxyPlot.Blazor/wwwroot/OxyPlot.Blazor.js
@@ -2,7 +2,12 @@
 window.OxyPlotBlazor = {
     getBoundingClientRect: function (e) {
         const r = e.getBoundingClientRect();
-        return [r.x, r.y, r.width, r.height];
+        // might return null in some cases for some values, which causes deserialization problems
+        // in blazor to double.json 
+        // https://github.com/belucha/oxyplot.blazor/issues/3
+        // see https://developer.mozilla.org/de/docs/Web/API/Element/getBoundingClientRect
+        // System.InvalidOperationException: Cannot get the value of a token type 'Null' as a number.
+        return [r.x??0, r.y??0, r.width??0, r.height??0];
     },
     disableContextMenu: function (element) {
         element.addEventListener('contextmenu', ev => {
