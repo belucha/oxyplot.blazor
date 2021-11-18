9b7c332feb89e7b1c1051862784668e2fef30af8
Daniel Gross <dgross@intronik.de>
2021-11-18 12:00:47 +0100

    catch TaskCanceledException

--- OxyPlot.Blazor/JsInterop.cs
+++ OxyPlot.Blazor/JsInterop.cs
@@ -16,8 +16,15 @@ namespace OxyPlot.Blazor
         /// <param name="JSRuntime"></param>
         public static async ValueTask<OxyRect> GetBoundingClientRectAsync(this ElementReference element, IJSRuntime JSRuntime)
         {
-            var o = await JSRuntime.InvokeAsync<double[]>("OxyPlotBlazor.getBoundingClientRect", element).ConfigureAwait(false);
-            return new OxyRect(left: o[0], top: o[1], width: o[2], height: o[3]);
+            try
+            {
+                var o = await JSRuntime.InvokeAsync<double[]>("OxyPlotBlazor.getBoundingClientRect", element).ConfigureAwait(false);
+                return new OxyRect(left: o[0], top: o[1], width: o[2], height: o[3]);
+            } catch (TaskCanceledException)
+            {
+                // just some dummy default, we want to avoid exceptions
+                return new OxyRect(left: 0, top: 0, width: 500, height: 300);
+            }
         }
         public static ValueTask DisableContextMenu(this ElementReference element, IJSRuntime JSRuntime)
             => JSRuntime.InvokeVoidAsync("OxyPlotBlazor.disableContextMenu", element);
