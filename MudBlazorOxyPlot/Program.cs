10b83313c28afec9363c2c3a3d7d2d55708ebf31
Daniel Gross <dgross@intronik.de>
2023-05-12 18:25:26 +0200

    add MudBlazorOxyPlot with dialog

---
+++ MudBlazorOxyPlot/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using MudBlazor.Services;
+using MudBlazorOxyPlot.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
+builder.Services.Configure<ResizeObserverOptions>(builder.Configuration.GetSection("ResizeObserverOptions"));
+builder.Services.AddOxyPlotBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
